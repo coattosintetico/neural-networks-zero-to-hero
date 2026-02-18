42ad52317db48516c534b22ce2d40d1adb189e05
coattosintetico <msanchezgonzalez99@gmail.com>
2026-02-18 20:51:53 +0100

    Progress on finishing (almost) lesson 2

--- run-jupyter-server.sh
+++ run-jupyter-server.sh
@@ -1,4 +1,5 @@
-uv run --with jupyter jupyter lab
+uv run --with jupyter jupyter lab --no-browser
 
-# there's another option for which i don't understand the difference
+# there's another option which seems to run jupyter **as a standalone tool**--we
+# don't want it, we want the jupyter lab in the same environment as this project
 # uv tool run jupyter lab
